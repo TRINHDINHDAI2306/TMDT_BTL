e95ff9ff590a20d418cae0184ae72f814c6655d0
TRINHDINHDAI2306 <94212576+TRINHDINHDAI2306@users.noreply.github.com>
2024-05-23 14:40:16 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongoDB: {
-    // uri: "mongodb+srv://trinhdinhdai1:Minhdat154!@cluster0.v6my26w.mongodb.net/tmdt?retryWrites=true&w=majority&appName=Cluster0",
-    uri: "mongodb://127.0.0.1:27017/TEST",
+    uri: "mongodb+srv://trinhdinhdai1:Minhdat154!@cluster0.v6my26w.mongodb.net/tmdt?retryWrites=true&w=majority&appName=Cluster0",
+    // uri: "mongodb://127.0.0.1:27017/TEST",
   },
 };
